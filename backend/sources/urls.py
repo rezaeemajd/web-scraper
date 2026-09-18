acd720c114ed996d12db992d0238609b7df6c034
Amir Majd <77883759+rezaeemajd@users.noreply.github.com>
2026-09-18 15:55:05 +0330

    feat(p0): add backend/sources/urls.py

---
+++ backend/sources/urls.py
@@ -0,0 +1,4 @@
+from rest_framework.routers import DefaultRouter
+from .views import SourceViewSet
+router=DefaultRouter(); router.register("",SourceViewSet,basename="source")
+urlpatterns=router.urls
