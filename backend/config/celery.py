db81052b60bf757d38666c550b0ca552d8efdb0a
Amir Majd <77883759+rezaeemajd@users.noreply.github.com>
2026-09-18 15:54:45 +0330

    feat(p0): add backend/config/celery.py

---
+++ backend/config/celery.py
@@ -0,0 +1,4 @@
+import os
+from celery import Celery
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","config.settings")
+app=Celery("cdi"); app.config_from_object("django.conf:settings",namespace="CELERY"); app.autodiscover_tasks()
